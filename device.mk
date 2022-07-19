1d50739148773f9f61fbb951893d5d77fa985988
Jake Weinstein <jake@aospa.co>
2022-07-19 15:34:28 -0500

    phone2: Remove inheriting of vendor-common
    We are not doing this.

--- device.mk
+++ device.mk
@@ -72,7 +72,6 @@ $(call inherit-product, build/make/target/product/gsi_keys.mk)
 BOARD_HAVE_BLUETOOTH := false
 BOARD_HAVE_QCOM_FM := false
 
-$(call inherit-product, device/qcom/vendor-common/common64.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 ###########
